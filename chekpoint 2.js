c537f55bad999b1d2659c79bd93f58ea5928b5dd
Sabrine <chaibisabrinemanouba@gmail.com>
2024-06-26 15:23:57 +0200

    update

--- chekpoint 2.js
+++ chekpoint 2.js
@@ -1,17 +1,19 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-    var colorBox = document.getElementById('color-box');
-    var changeColorBtn = document.getElementById('change-color-btn');
-     changeColorBtn.addEventListener('click', function() {
-        colorBox.style.backgroundColor = 'red';
-        
-      });
-      let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  const colorBox = document.getElementById('color-box');
+  const changeColorBtn = document.getElementById('change-color-btn');
+
+  function getRandomColor() {
+      const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
       let colorParts = [];
-     
-     for (let i = 0; i < 6; i++) {
-       colorParts.push(hexArray[Math.floor(Math.random() * hexArray.length)]);
-     }
-     
-     let finalColor = `#${colorParts.join("")}`;
-     colorBox.style.backgroundColor = finalColor;
-    });
+      for (let i = 0; i < 6; i++) {
+          colorParts.push(hexArray[Math.floor(Math.random() * hexArray.length)]);
+      }
+      return `#${colorParts.join("")}`;
+  }
+
+  changeColorBtn.addEventListener('click', function() {
+      const newColor = getRandomColor();
+      colorBox.style.backgroundColor = newColor;
+  });
+});
